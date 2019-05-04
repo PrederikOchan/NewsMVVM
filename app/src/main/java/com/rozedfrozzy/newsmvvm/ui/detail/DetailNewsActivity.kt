15fd6f6c11ff5cdee292c5666f6706bc60ef90a4
Roshan Din Firozi <roze.d.frozzy@gmail.com>
2019-05-04 10:57:47 +0700

    Initial commit

---
+++ app/src/main/java/com/rozedfrozzy/newsmvvm/ui/detail/DetailNewsActivity.kt
@@ -0,0 +1,13 @@
+package com.rozedfrozzy.newsmvvm.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rozedfrozzy.newsmvvm.R
+
+class DetailNewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_news)
+    }
+}
